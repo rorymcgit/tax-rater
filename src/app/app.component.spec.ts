5aba7cfdd0837d6e07ae48d7a4ff28a782e0367f
rorymcgit <rjmcguinness@gmail.com>
2025-10-25 11:59:41 +0100

    replace karma with jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tax-rater' title`, () => {
+  it(`should have the 'Effective Tax Rate Calculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tax-rater');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tax-rater');
+    expect(app.title).toEqual('Effective Tax Rate Calculator');
   });
 });
