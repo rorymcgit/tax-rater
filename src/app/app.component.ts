0f4bab698696b219443ff1e5a4c8a7380ffcc159
rorymcgit <rjmcguinness@gmail.com>
2025-10-25 09:27:16 +0100

    add highcharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { HighchartsChartModule } from 'highcharts-angular';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet], // , HighchartsChartModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
